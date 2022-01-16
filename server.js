d9de8f10ed8be4d13457c314bd4d7cc667c60550
Sabrina Zancotti <zancotti@gmail.com>
2022-01-16 15:11:15 +0100

    Added functionality for authentication and user for singin and signup

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
 import personData from "./data/persons.json";
 import filesData from "./data/files.json";
 import MailsData from "./data/mails.json";
+import bcrypt from "bcrypt-nodejs";
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -15,27 +17,44 @@ mongoose
   .connect(mongoUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then((res) => console.log("Connected to DB"))
   .catch((err) => console.log("Something went wrong!", err));
 mongoose.Promise = Promise;
 
-const port = process.env.PORT || 8080;
-const app = express();
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: { type: String, required: true },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
 
-const Person = mongoose.model("Person", {
+const User = mongoose.model("User", UserSchema);
+
+const PersonSchema = new mongoose.Schema({
   id: Number,
   firstName: String,
   lastName: String,
 });
 
-const File = mongoose.model("File", {
+const Person = mongoose.model("Person", PersonSchema);
+
+const FileSchema = new mongoose.Schema({
   id: Number,
   fileId: String,
   name: String,
 });
 
-const Mail = mongoose.model("Mail", {
+const File = mongoose.model("User", FileSchema);
+
+const MailSchema = new mongoose.Schema({
   id: Number,
   subject: String,
   text: String,
@@ -44,8 +63,11 @@ const Mail = mongoose.model("Mail", {
   timeStamp: Date,
 });
 
+const Mail = mongoose.model("Mail", MailSchema);
+
 if (process.env.RESET_DATABASE) {
   const seedDatabase = async () => {
+    await User.deleteMany({});
     await Person.deleteMany({});
     await File.deleteMany({});
     await Mail.deleteMany({});
@@ -68,24 +90,123 @@ if (process.env.RESET_DATABASE) {
   seedDatabase();
 }
 
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken });
+
+    if (user) {
+      next();
+    } else {
+      res.status(404).json({ response: "Please, log in", success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+};
+
+const initGuest = async () => {
+  const guest = await User.findOne({ email: "guest@guest.com" });
+  if (!guest) {
+    const guest = new User({
+      email: "guest@guest.com",
+      password: "guest",
+    });
+    await guest.save();
+  }
+};
+
+initGuest();
+
+const port = process.env.PORT || 8080;
+const app = express();
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/media", express.static("public"));
 
 // Start defining your routes here
+
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    if (password.length < 5) {
+      throw "Password must be at least 5 characters long";
+    }
+    const newUser = new User({
+      email: email.toLowerCase(),
+      password: bcrypt.hashSync(password, salt),
+    });
+
+    await newUser.save();
+    res.status(201).json({
+      response: {
+        id: newUser._id,
+        accessToken: newUser.accessToken,
+        email: newUser.email,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (user.email === "guest@guest.com") {
+      return res.status(200).json({
+        response: {
+          Id: user._id,
+          email: email,
+          accessToken: user.accessToken,
+        },
+        success: true,
+      });
+    }
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      return res.status(200).json({
+        response: {
+          Id: user._id,
+          email: email,
+          accessToken: user.accessToken,
+        },
+        success: true,
+      });
+    } else {
+      res.status(404).json({
+        response: "User email or password doesn't match",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(404).json({ response: error, success: false });
+  }
+});
+
+app.get("/mails", authenticateUser);
+app.get("/mails", async (req, res) => {
+  const mails = await Mail.find();
+  res.json(mails);
+});
+
+app.get("/persons", authenticateUser);
 app.get("/persons", async (req, res) => {
   const persons = await Person.find();
   res.json(persons);
 });
 
+app.get("/files", authenticateUser);
 app.get("/files", async (req, res) => {
   const files = await File.find();
   res.json(files);
-});
-
-app.get("/mails", async (req, res) => {
-  const mails = await Mail.find();
-  res.json(mails);
 });
 
 // Start the server
